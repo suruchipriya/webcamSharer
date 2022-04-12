3cca98bc784f0476b8ef7023a55bd15cda9509b8
suruchipriya <suruchi.wings29@gmail.com>
2022-04-12 19:29:48 +0530

    file link sharing logic

--- filesharer.py
+++ filesharer.py
@@ -2,6 +2,12 @@ from filestack import Client
 
 
 
-class fileSharer:
-
-def __init__(self, filepath, api_key=)
+class FileSharer:
+  def __init__(self, filepath, api_key="AZW1OWaQVSaatWyesIkWDz"):
+      self.filepath = filepath
+      self.api_key = api_key
+  
+  def share(self):
+      client = Client(self.api_key)
+      new_filelink = client.upload(filepath=self.filepath)
+      return new_filelink.url
